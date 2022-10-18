931903fb1c3bd3346ee6cfd8be5aacdb96411024
GreGuarino66 <gguarino22@fed.idserve.net>
2022-10-18 14:49:10 +0200

    Initial Commit

---
+++ learntogether/learntogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  learntogetherApp.swift
+//  learntogether
+//
+//  Created by Gregorio Guarino on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct learntogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
